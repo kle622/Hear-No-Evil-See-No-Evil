efb4cbb5a4d57c47119fb201a7df178d8718c7a7
cclark18 <cclark18@calpoly.edu>
2015-04-23 09:58:51 -0700

    updating camera to move around player freely

--- source/Camera/Camera3DPerson.cpp
+++ source/Camera/Camera3DPerson.cpp
@@ -3,8 +3,10 @@
 Camera3DPerson:Camera3DPerson(Handles *handles, GameObject *focus, float zoom, float fov, float aspect, float _near, float _far)
 {
   // when both angles are 0, the camera is directly behind the player, horizontal
-  this->pitch = 0;  // increase pitch to look down
+  this->phi = 0;  // increase phi to look down
+  this->theta = 0;  // increase theta to look left
   this->focus = focus;
+  this->zoom = zoom;
   Camera(handles,
          focus->position,
          getEye(),
@@ -14,24 +16,37 @@ Camera3DPerson:Camera3DPerson(Handles *handles, GameObject *focus, float zoom, f
 
 glm::vec3 Camera3DPerson:getEye()
 {
-  glm::vec3 res = this->focus->direction;
+  /*glm::vec3 res = this->focus->direction;
   res.y = 0;
   res = glm::normalize(res);
   float theta = acos(glm::dot(res, glm::vec3(1.0f, 0.0f, 0.0f)));   // theta = 0 points along positive x
   if (res.z < 0) {  // theta increases towards positive z
     theta *= -1.0f;
   }
-  res = glm::vec3(cos(theta) * cos(pitch),
-                  sin(pitch),
-                  sin(theta) * cos(pitch));
+  res = glm::vec3(cos(theta) * cos(phi),
+                  sin(phi),
+                  sin(theta) * cos(phi));
   res *= this->zoom;
   res += this->focus->position;
+  return res;*/
+  glm::vec3 res = glm::vec3(cos(theta) * cos(phi),
+                            sin(phi),
+                            sin(theta) * cos(phi));
+  res *= this->zoom;
   return res;
 }
 
+// positive step looks up
 void Camera3DPerson::moveVert(float step)
 {
-  this->pitch -= step;
+  this->phi -= step;
+  this->eye = getEye();
+}
+
+// positive step looks right
+void Camera3DPerson::moveHoriz(float step)
+{
+  this->theta -= step;
   this->eye = getEye();
 }
 
