76e3b75c0df6758c5197fa2570df67c52e5af04e
cclark18 <cclark18@calpoly.edu>
2015-05-03 13:07:41 -0700

    working on debug shader

--- source/DebugDraw/DebugDraw.h
+++ source/DebugDraw/DebugDraw.h
@@ -1,7 +1,7 @@
-
 #ifndef __DEBUG_DRAW_H
 #define __DEBUG_DRAW_H
-#include "../GameObject/Handles.h"
+#include "DebugHandles.h"
+#include <string>
 
 typedef struct LineSegment
 {
@@ -19,14 +19,24 @@ typedef struct
 class DebugDraw
 {
   public:
-    Handles *handles;
-    DebugDraw(Handles *handles);
+    DebugHandles handles;
+    DebugDraw();
+    DebugDraw(const std::string &vShaderName, const std::string &fShaderName);
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 projection;
     void addLine(LineSegment line);
+    void addLine(glm::vec3 start, glm::vec3 end);
     void addRing(Ring3D ring);
-    void addPolygon(int numVertices, ...);
+    void addRing(glm::vec3 center, float radius, glm::vec3 normal);
     void drawAll();
+    //void addPolygon(int numVertices, ...);
   private:
     std::vector<LineSegment> lines;
     std::vector<Ring3D> rings;
+    std::vector<float> posBuf;
+    std::vector<float> colBuf;
+    GLuint posBufObj;
+    GLuint colBufObj;
 };
 #endif
