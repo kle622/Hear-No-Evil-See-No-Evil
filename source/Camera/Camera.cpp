070d6a174b7f3b5b21fc6a41c2e6b14523ff8747
cclark18 <cclark18@calpoly.edu>
2015-05-11 10:48:28 -0700

    cleaned up camera code to use one loop

--- source/Camera/Camera.cpp
+++ source/Camera/Camera.cpp
@@ -65,14 +65,6 @@ std::vector<std::shared_ptr<GameObject>> Camera::getUnculled(WorldGrid *worldgri
   // the plane normals are not normalized, and point to the inside of the view frustum
   // plane equations obtained from http://gamedevs.org/uploads/fast-extraction-viewing-frustum-planes-from-world-view-projection-matrix.pdf
   // a plane is represented by a vec4 as <a, b, c, d>
-  /*glm::vec4 left = glm::vec4(VP[3][0] + VP[0][0], VP[3][1] + VP[0][1], VP[3][2] + VP [0][2], VP[3][3] + VP[0][3]);
-  glm::vec4 right = glm::vec4(VP[3][0] - VP[0][0], VP[3][1] - VP[0][1], VP[3][2] - VP [0][2], VP[3][3] - VP[0][3]);
-  glm::vec4 bottom = glm::vec4(VP[3][0] + VP[1][0], VP[3][1] + VP[1][1], VP[3][2] + VP [1][2], VP[3][3] + VP[1][3]);
-  glm::vec4 top = glm::vec4(VP[3][0] - VP[1][0], VP[3][1] - VP[1][1], VP[3][2] - VP [1][2], VP[3][3] - VP[1][3]);
-  glm::vec4 near = glm::vec4(VP[3][0] + VP[2][0], VP[3][1] + VP[2][1], VP[3][2] + VP [2][2], VP[3][3] + VP[2][3]);
-  glm::vec4 far = glm::vec4(VP[3][0] - VP[2][0], VP[3][1] - VP[2][1], VP[3][2] - VP [2][2], VP[3][3] - VP[2][3]);*/
-  
-  // seems to be correct view frustum
   glm::vec4 left = glm::vec4(VP[0][3] + VP[0][0], VP[1][3] + VP[1][0], VP[2][3] + VP [2][0], VP[3][3] + VP[3][0]);
   glm::vec4 right = glm::vec4(VP[0][3] - VP[0][0], VP[1][3] - VP[1][0], VP[2][3] - VP [2][0], VP[3][3] - VP[3][0]);
   glm::vec4 bottom = glm::vec4(VP[0][3] + VP[0][1], VP[1][3] + VP[1][1], VP[2][3] + VP [2][1], VP[3][3] + VP[3][1]);
@@ -82,13 +74,14 @@ std::vector<std::shared_ptr<GameObject>> Camera::getUnculled(WorldGrid *worldgri
 
 #ifdef DEBUG
   this->debug->addBox(left, right, bottom, top, near, far, glm::vec3(0.99f, 0.85f, 0.55f), false);
-#endif
+
   /*std::cout << "left: <" << left.x << ", " << left.y << ", " << left.z << ", " << left.w << ">" << std::endl;
   std::cout << "right: <" << right.x << ", " << right.y << ", " << right.z << ", " << right.w << ">" << std::endl;
   std::cout << "bottom: <" << bottom.x << ", " << bottom.y << ", " << bottom.z << ", " << bottom.w << ">" << std::endl;
   std::cout << "top: <" << top.x << ", " << top.y << ", " << top.z << ", " << top.w << ">" << std::endl;
   std::cout << "near: <" << near.x << ", " << near.y << ", " << near.z << ", " << near.w << ">" << std::endl;
   std::cout << "far: <" << far.x << ", " << far.y << ", " << far.z << ", " << far.w << ">" << std::endl;*/
+#endif
 
   planes.push_back(left); // left
   planes.push_back(right); // right
@@ -97,9 +90,9 @@ std::vector<std::shared_ptr<GameObject>> Camera::getUnculled(WorldGrid *worldgri
   planes.push_back(near); // near
   planes.push_back(far); // far
 
-  // TODO figure out how to draw view frustum wireframe
-
   std::vector<std::shared_ptr<GameObject>> allObjects = worldgrid->list;
+  std::vector<std::shared_ptr<GameObject>> walls = worldgrid->wallList;
+  allObjects.insert(allObjects.begin(), walls.begin(), walls.end());  // whyyyyyyyyyyy
   std::vector<std::shared_ptr<GameObject>> inVF;
   for (auto objIter = allObjects.begin(); objIter != allObjects.end(); ++objIter) {
     OBB *obb = new OBB((*objIter)->position, (*objIter)->dimensions);
@@ -109,9 +102,6 @@ std::vector<std::shared_ptr<GameObject>> Camera::getUnculled(WorldGrid *worldgri
     bool pass = true;
     for (auto planeIter = planes.begin(); pass && planeIter != planes.end(); ++planeIter) {
       pass = obbOutsidePlane(*obb, *planeIter);
-      /*if (!obbOutsidePlane(*obb, *planeIter)) {
-        pass = false;
-      }*/
     }
     if (pass) {
       inVF.push_back(*objIter);
@@ -119,7 +109,7 @@ std::vector<std::shared_ptr<GameObject>> Camera::getUnculled(WorldGrid *worldgri
   }
 
   // ----- Wall VFC -----
-  std::vector<std::shared_ptr<GameObject>> walls = worldgrid->wallList;
+  /*std::vector<std::shared_ptr<GameObject>> walls = worldgrid->wallList;
   for (auto objIter = walls.begin(); objIter != walls.end(); ++objIter) {
     OBB *obb = new OBB((*objIter)->position, (*objIter)->dimensions);
 #ifdef DEBUG
@@ -128,14 +118,11 @@ std::vector<std::shared_ptr<GameObject>> Camera::getUnculled(WorldGrid *worldgri
     bool pass = true;
     for (auto planeIter = planes.begin(); pass && planeIter != planes.end(); ++planeIter) {
       pass = obbOutsidePlane(*obb, *planeIter);
-      /*if (!obbOutsidePlane(*obb, *planeIter)) {
-        pass = false;
-      }*/
     }
     if (pass) {
       inVF.push_back(*objIter);
     }
-  }
+  }*/
 
   // ----- Occlusion Culling -----
   std::vector<std::shared_ptr<GameObject>> unculled;
@@ -146,13 +133,13 @@ std::vector<std::shared_ptr<GameObject>> Camera::getUnculled(WorldGrid *worldgri
   return unculled;
 }
 
-// TODO implement (pseudocode in book)
-// p. 755-757
-// returns true if object is completely inside or intersects plane
-// returns false if object is completely outside plane
-//
-// inside is defined as positive, outside is defined as negative
-// plane is not assumed to be normalized
+/* p. 755-757
+ * returns true if object is completely inside or intersects plane
+ * returns false if object is completely outside plane
+ *
+ * inside is defined as positive, outside is defined as negative
+ * plane is not assumed to be normalized
+ */
 bool Camera::obbOutsidePlane(OBB obb, glm::vec4 plane)
 {
   float nlength = glm::length(glm::vec3(plane.x, plane.y, plane.z));
