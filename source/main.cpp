bbf869690dc9596d6d23be2b0aa9e831c7abb31d
lanahodzic <lana@enoteway.com>
2015-05-14 13:41:41 -0700

    updated main

--- source/main.cpp
+++ source/main.cpp
@@ -445,8 +445,8 @@ void drawPass1(WorldGrid* gameObjects) {
   //SetDepthMVP(true, ground->position, ground->rotation, ground->scale);
   //pass1Handles.draw(ground);
   vector<shared_ptr<GameObject>> drawList = gameObjects->list;
-  //vector<shared_ptr<GameObject>> walls = gameObjects->wallList;
-  //drawList.insert(drawList.end(), walls.begin(), walls.end());
+  vector<shared_ptr<GameObject>> walls = gameObjects->wallList;
+  drawList.insert(drawList.end(), walls.begin(), walls.end());
   for (int i = 0; i < drawList.size(); i++) {
     SetDepthMVP(true, drawList[i]->position, drawList[i]->rotation, drawList[i]->scale);
     pass1Handles.draw(drawList[i].get());
