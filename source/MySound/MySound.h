a403a0a798fcb0b68aa44f5bfa2178c5f2ff27c7
ericthorndyke <ethorndy@gmail.com>
2015-05-14 17:38:52 -0700

    changed the include files

--- source/MySound/MySound.h
+++ source/MySound/MySound.h
@@ -12,20 +12,13 @@
 #include "../glm/glm.hpp"
 #include "../glm/gtc/matrix_transform.hpp" //perspective, trans etc
 #include "../glm/gtc/type_ptr.hpp" //value_ptr
+#include "conio.h"
+#include "irrKlang.h"
 
 #if defined WIN32
 #include <windows.h>
-#include <conio.h>
 inline void sleepSomeTime() { Sleep(100); }
-#pragma comment(lib, "../dependencies/irrKlang/lib/Winx64-visualStudio/irrKlang.lib") // link with irrKlang.dll
-#else
-#include "../dependencies/irrKlang/examples/common/conio.h"
-//#pragma comment(lib, "../dependencies/irrKlang/lib/irrKlang.lib") // link with irrKlang.dll
 #endif
-//#if defined __APPLE__ && __MACH__
-//#include "..dependencies/irrKlang/examples/common/conio.h"
-//#endif
-#include "../../dependencies/irrKlang/include/irrKlang.h"
 
 using namespace irrklang;
 using namespace glm;
