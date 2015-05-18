845258527d5296a7525ea27628e9e229f37f4374
cclark18 <cclark18@calpoly.edu>
2015-05-18 01:36:40 -0700

    DEBUGGING

--- source/Camera/DetectionCamera.cpp
+++ source/Camera/DetectionCamera.cpp
@@ -2,11 +2,12 @@
 #define _USE_MATH_DEFINES
 
 #define DEBUG
+#define VERT_OFFSET 0.8f
 
 DetectionCamera::DetectionCamera(WorldGrid *world, GameObject *viewer, GameObject *target,
-                                 float fov, float aspect, float _near, float _far, DebugDraw *debug) :
-                                 Camera(target->position, viewer->position + glm::vec3(0.0f, 2.0f, 0.0f),
-                                 glm::vec3(0.0f, 1.0f, 0.0f), fov, aspect, _near, _far, debug)
+    float fov, float aspect, float _near, float _far, DebugDraw *debug) :
+  Camera(target->position, viewer->position + glm::vec3(0.0f, VERT_OFFSET, 0.0f),
+      glm::vec3(0.0f, 1.0f, 0.0f), fov, aspect, _near, _far, debug)
 {
   this->world = world;
   this->viewer = viewer;
@@ -15,8 +16,8 @@ DetectionCamera::DetectionCamera(WorldGrid *world, GameObject *viewer, GameObjec
 
 void DetectionCamera::update()
 {
-  this->eye = this->viewer->position + glm::vec3(0.0f, 2.0f, 0.0f);
-  this->lookat = this->viewer->position + this->viewer->direction;
+  this->eye = this->viewer->position + glm::vec3(0.0f, VERT_OFFSET, 0.0f);
+  this->lookat = this->viewer->position + this->viewer->direction + glm::vec3(0.0f, VERT_OFFSET, 0.0f);
 }
 
 float DetectionCamera::percentInView()
@@ -27,6 +28,9 @@ float DetectionCamera::percentInView()
   std::vector<glm::vec3> *corners = targetBox->getCorners();
   std::vector<glm::vec4> *planes = getViewFrustum(getProjection() * getView());
   std::vector<glm::vec3> cornersInView;
+#ifdef DEBUG
+  this->debug->addBox(planes->at(0), planes->at(1), planes->at(2), planes->at(3), planes->at(4), planes->at(5), glm::vec3(0.99f, 0.85f, 0.55f), false, false);
+#endif
   for (auto cornerItr = corners->begin(); cornerItr != corners->end(); ++cornerItr) {
     bool insideFrustum = true;
     for (auto planeItr = planes->begin(); insideFrustum && planeItr != planes->end(); ++planeItr) {
@@ -51,31 +55,35 @@ float DetectionCamera::percentInView()
   objInView.insert(objInView.begin(), walls.begin(), walls.end());  // whyyyyyyyyyyy
 #endif
   float incr = 1.0f / cornersInView.size();
-  for (auto objIter = objInView.begin(); objIter != objInView.end(); ++objIter) {
-    // don't check collisions against guards and player
-    if (NULL == dynamic_pointer_cast<Guard>(*objIter) && NULL == dynamic_pointer_cast<Player>(*objIter)) {
-    //if (objIter->get() != this->viewer) { // make sure not to block view with yourself!
-      for (auto cornerItr = cornersInView.begin(); cornerItr != cornersInView.end(); ++cornerItr) {
+  for (auto cornerItr = cornersInView.begin(); cornerItr != cornersInView.end(); ++cornerItr) {
 #ifdef DEBUG
-        this->debug->addLine(this->eye, *cornerItr, glm::vec3(0.0f, 0.0f, 1.0f), false);
+    this->debug->addLine(this->eye, *cornerItr, glm::vec3(0.0f, 0.0f, 1.0f), false);
 #endif
+    bool cornerInView = true;
+    float rayDist;
+    for (auto objIter = objInView.begin(); cornerInView && objIter != objInView.end(); ++objIter) {
+      // TODO don't check collisions against viewer and target
+      // doesn't work yet
+      //if (NULL == dynamic_pointer_cast<Guard>(*objIter) && NULL == dynamic_pointer_cast<Player>(*objIter)) {
+      if ((this->viewer) != ((*objIter).get()) && (this->target) != ((*objIter).get())) {
+      //if (true) {
         OBB *hitBox = new OBB((*objIter)->position, (*objIter)->dimensions);
-        float rayDist;
         if (rayOBBIntersect(&rayDist, (*objIter)->position, *cornerItr - this->eye, *hitBox)) {
           float lookDist = glm::distance(*cornerItr, this->eye);
           if (lookDist > rayDist) {
-#ifdef DEBUG
-            this->debug->addLine(this->eye, this->eye + glm::normalize(*cornerItr - this->eye) * rayDist, glm::vec3(1.0f, 0.0f, 0.0f), true);
-#endif
-            result += incr;
+            cornerInView = false;
           }
-        }
-        else {
-          result += incr;
         }
       }
     }
+#ifdef DEBUG
+    if (!cornerInView) {
+      this->debug->addLine(this->eye, this->eye + glm::normalize(*cornerItr - this->eye) * rayDist, glm::vec3(1.0f, 0.0f, 0.0f), true);
+    }
+#endif
+    if (cornerInView) {
+      result += incr;
+    }
   }
-
   return result;
 }
