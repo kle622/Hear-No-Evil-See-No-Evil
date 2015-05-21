862dfb7e421596c21376b4b60f51a533f321f40e
Kevin Le <kle622@gmail.com>
2015-05-21 00:17:52 -0700

    started hud problems

--- source/HUD/Hud.h
+++ source/HUD/Hud.h
@@ -1,8 +1,23 @@
 #ifndef HUD_H_
 #define HUD_H_
 
-#include GameObject.h
+#include "../GameObject/GameObject.h"
+#include "../GameObject/Shape.h"
+
+#include <cmath>
+#include <vector>
+#include <memory>
+
+using namespace std;
 
 class Hud {
-	
-}
+	public:
+		shared_ptr<Shape> stamina;
+		shared_ptr<Shape> detection;
+		shared_ptr<Shape> dialogue;
+		void updateStaminaHud(float stamina);
+		void updateDetectionHud(float detection);
+		void updateDialogueHud(int texture);
+};
+
+#endif
