9ba3c62b934a27234cc681b710aff42a32a1df9e
Johnathan Baird <janimationd@gmail.com>
2015-06-09 19:37:44 -0700

    added some triangle stuff

--- source/main.cpp
+++ source/main.cpp
@@ -1214,7 +1214,7 @@ void initPlayer(WorldGrid* gameObjects) {
 
   gameObjects->add(shared_ptr<GameObject>(playerObject));
 }
-#define GUARD_Y_SHIFT 0.8f
+#define GUARD_Y_SHIFT 1.4f
 void initGuards(WorldGrid* gameObjects) {
   vector<PathNode> guardPath;
   FILE *file = fopen(resPath("GuardPaths.txt").data(), "r");
