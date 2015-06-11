5a696e4e1f06416af2665a10bf7c2190485aebff
cclark18 <cclark18@calpoly.edu>
2015-06-10 22:16:05 -0700

    homogenous divide

--- resources/shaders/geometryFrag.glsl
+++ resources/shaders/geometryFrag.glsl
@@ -10,5 +10,5 @@ void main() {
   gl_FragData[0] = texture2D(texture, vTexCoord); // diffuse
   gl_FragData[1] = vec4(vWorldPos, 1.0); // world pos
   gl_FragData[2] = vec4(normalize(vNormal), 1.0); // normal
-  gl_FragData[3] = vec4(vShadowCoord.z, vShadowCoord.z, vShadowCoord.z, 1.0);
+  gl_FragData[3] = vec4(vShadowCoord.z / vShadowCoord.w, vShadowCoord.z / vShadowCoord.w, vShadowCoord.z / vShadowCoord.w, 1.0);
 }
