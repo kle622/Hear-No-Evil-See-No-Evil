7451792cbbf680e84322e49bc3d651f20e746a03
cclark18 <cclark18@calpoly.edu>
2015-06-10 19:55:36 -0700

    removed quad loveliness

--- source/main.cpp
+++ source/main.cpp
@@ -84,53 +84,6 @@ using namespace glm;
 //  glm::vec3 coneDirection;
 //};
 
-//#define QUAD_BULLSHIT
-#ifdef QUAD_BULLSHIT
-GLuint quad_VertexArrayID;
-static const GLfloat g_quad_vertex_buffer_data[] = {
-    -1.0f, -1.0f, 0.0f,
-    1.0f, -1.0f, 0.0f,
-    -1.0f,  1.0f, 0.0f,
-    -1.0f,  1.0f, 0.0f,
-    1.0f, -1.0f, 0.0f,
-    1.0f,  1.0f, 0.0f,
-};
-/*static const GLfloat g_quad_vertex_buffer_data[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.5f,  0.5f, 0.0f,
-};*/
-static const GLfloat g_quad_uv_buffer_data[] = {
-    0.0f, 0.0f,
-    1.0f, 0.0f,
-    0.0f, 1.0f,
-    0.0f, 1.0f,
-    1.0f, 0.0f,
-    1.0f, 1.0f,
-};
-/*static const GLfloat g_quad_uv_buffer_data[] = {
-    0.0f, 0.0f,
-    0.5f, 0.0f,
-    0.0f, 0.5f,
-    0.0f, 0.5f,
-    0.5f, 0.0f,
-    0.5f, 0.5f,
-};*/
-/*static const GLfloat g_quad_uv_buffer_data[] = {
-    0.0f, 0.0f,
-    2.0f, 0.0f,
-    0.0f, 2.0f,
-    0.0f, 2.0f,
-    2.0f, 0.0f,
-    2.0f, 2.0f,
-};*/
-GLuint quad_vertexbuffer;
-GLuint quad_uvbuffer;
-#endif
-
 vector<Light> gLights;
 
 vector<tinyobj::shape_t> player;
@@ -913,20 +866,6 @@ void geometryPass(WorldGrid* gameObjects, float time) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }*/
 
-#ifdef QUAD_BULLSHIT
-void drawQuad()
-{
-  // draw shape
-  GLSL::enableVertexAttribArray(lightHandles.aPosition);
-  glBindVertexArray(quad_VertexArrayID);
-  glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
-  glVertexAttribPointer(lightHandles.aPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
-  checkGLError();
-
-  glDrawArrays(GL_TRIANGLES, 0, sizeof(g_quad_vertex_buffer_data) * 3 * 6);
-}
-#endif
-
 void lightPass() {
   glUseProgram(lightHandles.prog);
   glDisable(GL_CULL_FACE);
@@ -1583,21 +1522,6 @@ void initWalls(WorldGrid* gameObjects) {
 
 }
 
-#ifdef QUAD_BULLSHIT
-void initQuad() 
-{
-  glGenVertexArrays(1, &quad_VertexArrayID);
-  assert(quad_VertexArrayID > 0);
-  glBindVertexArray(quad_VertexArrayID);
-
-  glGenBuffers(1, &quad_vertexbuffer);
-  assert(quad_vertexbuffer > 0);
-  glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_quad_vertex_buffer_data) * sizeof(GLfloat), &g_quad_vertex_buffer_data[0], GL_STATIC_DRAW);
-  
-}
-#endif
-
 int main(int argc, char **argv)
 {
     // Sound Object
@@ -1753,10 +1677,6 @@ int main(int argc, char **argv)
     curveOutput.open(resPath("introCurveSample.txt"));
 #endif
 
-#ifdef QUAD_BULLSHIT
-    initQuad();
-#endif
-    
     double timeCounter = 0;
     
     //  printf("shadow map id: %d\n", shadowMap);
